68d502407a2fc1b5f33d9aff13e68f6cc7bb9510
andre <kinosite@gmail.com>
2025-06-20 11:34:14 +0300

    added tests: testValidPassword, testSymbolsLessThan8, testInvalidSpecSymbol, testNoUppercase

--- src/validator/tests/PasswordValidatorTest.java
+++ src/validator/tests/PasswordValidatorTest.java
@@ -1,5 +1,42 @@
 package validator.tests;
 
+import org.junit.jupiter.api.Test;
+import validator.utils.PasswordValidator;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+
 public class PasswordValidatorTest {
+    @Test
+    void testValidPassword(){
+        assertTrue(PasswordValidator.isStrong("Qwerty0!"));
+        assertTrue(PasswordValidator.isStrong("Qwerty0?"));
+        assertTrue(PasswordValidator.isStrong("Qwerty0@"));
+        assertTrue(PasswordValidator.isStrong("Qwerty0#"));
+
+        assertTrue(PasswordValidator.isStrong("A1b2c3d!"));
+        assertTrue(PasswordValidator.isStrong("A1b2c3d4e5f6g7h8i9J!"));
+    }
+
+    @Test
+    void testSymbolsLessThan8(){
+        //TODO
+        // Меньше 8 символов, но по составу подходящие (буквы, цифра, спецсимвол)
+        assertFalse(PasswordValidator.isStrong("Qwert0!"));  // 7 символов
+    }
+
+    @Test
+    void testInvalidSpecSymbol(){
+        assertFalse(PasswordValidator.isStrong("Qwerty0 "));
+        assertFalse(PasswordValidator.isStrong("Qwe^rty0"));
+        assertFalse(PasswordValidator.isStrong("~Qwerty0"));
+    }
+
+    @Test
+    void testNoUppercase() {
+        // Пароль содержит строчные, цифру, спецсимвол, но нет заглавной буквы
+        assertFalse(PasswordValidator.isStrong("qwerty2!"));
+    }
 
 }
--- src/validator/utils/PasswordValidator.java
+++ src/validator/utils/PasswordValidator.java
@@ -1,7 +1,25 @@
 package validator.utils;
 
 public class PasswordValidator {
-    public static boolean isStrong (String password){
-        return false;
+    public static boolean isStrong(String password) {
+        if (password == null) return false;
+        int length = password.length();
+        if (length < 8 || length > 20) return false;
+
+        boolean hasLower = false;
+        boolean hasUpper = false;
+        boolean hasDigit = false;
+        boolean hasSpecial = false;
+        String specialChars = "!@#$%^&*()-_+=";
+
+        for (char c : password.toCharArray()) {
+            if (Character.isLowerCase(c)) hasLower = true;
+            else if (Character.isUpperCase(c)) hasUpper = true;
+            else if (Character.isDigit(c)) hasDigit = true;
+            else if (specialChars.indexOf(c) >= 0) hasSpecial = true;
+            else return false; // запрещённый символ
+        }
+
+        return hasLower && hasUpper && hasDigit && hasSpecial;
     }
 }
